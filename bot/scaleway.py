99eb9d1cdfddc226c898578b27c4d92138f5e945
Alessio Margelli <alex9446@gmail.com>
2026-08-19 18:44:12 +0200

    split functions for future implementation

--- bot/scaleway.py
+++ bot/scaleway.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 from scaleway_async import ALL_ZONES, Client
 from scaleway_async.instance.v1.api import InstanceV1API
-from scaleway_async.instance.v1.types import ServerAction
+from scaleway_async.instance.v1.types import Server, ServerAction
 
 SERVERACTIONS = Literal['poweron', 'poweroff']
 
@@ -32,11 +32,21 @@ class Scaleway:
                                 for zone in ALL_ZONES])
         return [item for sublist in server for item in sublist]
 
-    async def perform_action(self, action: SERVERACTIONS, server_id: UUID):
+    async def find_server_by_id(self, server_id: UUID):
         servers = await self.list_servers()
         server = next((s for s in servers if s.id == server_id), None)
         if not server:
             raise ValueError(f'no server with id: {server_id}')
+        return server
+
+    async def find_server_by_name(self, server_name: str):
+        servers = await self.list_servers()
+        server = next((s for s in servers if s.name == server_name), None)
+        if not server:
+            raise ValueError(f'no server with name: {server_name}')
+        return server
+
+    async def perform_action(self, action: SERVERACTIONS, server: Server):
         server_state = server.state
         if ((action == 'poweron' and server_state != 'stopped') or
                 (action == 'poweroff' and server_state != 'running')):
@@ -44,7 +54,7 @@ class Scaleway:
 
         await self.instance_api.server_action(
             action=ServerAction(action),
-            server_id=str(server_id),
+            server_id=server.id,
             zone=server.zone
         )
 
@@ -54,5 +64,6 @@ class Scaleway:
             raise ValueError('action not valid')
         if not is_uuid_v4(server_id):
             raise ValueError('server_id not valid')
-        await self.perform_action(action, server_id)
+        server = await self.find_server_by_id(server_id)
+        await self.perform_action(action, server)
         return action
